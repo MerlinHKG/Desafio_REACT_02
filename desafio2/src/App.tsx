64601b7d9df461d37d1753c6b8195666e4030f47
Merlin <git.qodeless@outlook.com>
2021-11-19 19:45:09 -0300

    Desafio 2 concluido

---
+++ desafio2/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Contador from './components/Contador'
+
+function App() {
+  return (
+    <body>
+    <div className="App">
+      <Contador></Contador>
+    </div>
+    </body>
+  );
+}
+
+export default App;
